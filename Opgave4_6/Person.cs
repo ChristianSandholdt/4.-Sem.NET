9d045e240eaf44883c4f37694089210edb1b5f4a
ChristianSandholdt <100411100+ChristianSandholdt@users.noreply.github.com>
2023-09-01 14:08:30 +0200

    Opgave 4_6

---
+++ Opgave4_6/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave4_6
+{
+    internal class Person : IComparer<Person>
+    {
+        public int Compare(Person x, Person y)
+        {
+            throw new NotImplementedException();
+        }
+        public String Name { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public Person(String name, int age, double weight)
+        {
+            Name = name;
+            Age = age;
+            Weight = weight;
+        }
+        public override String ToString()
+        {
+            return "Person: " + Name + " Age: " + Age + " Weight: " + Weight;
+        }
+
+        public static void printList(List<Person> list)
+        {
+            foreach (Person p in list)
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+    }
+}
---
+++ Opgave4_6/Program.cs
@@ -0,0 +1,35 @@
+﻿using Opgave4_6;
+using static Opgave4_6.Person;
+
+Person p1 = new Person("Martin", 11, 48);
+Person p2 = new Person("Hans", 12, 47);
+Person p3 = new Person("Jens", 13, 41);
+Person p4 = new Person("Ib", 14, 42);
+Person p5 = new Person("Victor", 15, 43.5);
+Person p6 = new Person("Anders", 13, 43.4);
+
+List<Person> list = new List<Person>();
+
+list.Add(p1);
+list.Add(p2);
+list.Add(p3);
+list.Add(p4);
+list.Add(p5);
+list.Add(p6);
+
+list.Sort(new ByAgeSorter());
+Console.WriteLine("Sorted by age");
+printList(list);
+
+list.Sort(new ByNameSorter());
+Console.WriteLine("Sorted by name");
+printList(list);
+
+list.Sort(new ByWeightSorter());
+Console.WriteLine("Sorted by weight");
+printList(list);
+
+
+
+
+
