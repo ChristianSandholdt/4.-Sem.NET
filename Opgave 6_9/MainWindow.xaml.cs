6b7f73e67872611a7015c7cddae12033439bf808
ChristianSandholdt <cks2407@gmail.com>
2023-09-12 14:51:52 +0200

    Opgaver til lektion 6

---
+++ Opgave 6_9/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Opgave_6_9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void leftAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (leftTextbox.Text.Length > 0)
+            {
+                String s = leftTextbox.Text;
+                leftListBox.Items.Add(s);
+                leftTextbox.Clear();
+            }
+        }
+
+        private void rightAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (rightTextBox.Text.Length > 0)
+            {
+                String s = rightTextBox.Text;
+                rightListBox.Items.Add(s);
+                rightTextBox.Clear();
+            }
+        }
+
+        private void leftToRightArrow_Click(object sender, RoutedEventArgs e)
+        {
+            List<int> indexes = new List<int>();
+            foreach (String s in leftListBox.SelectedItems)
+            {
+                rightListBox.Items.Add(s);
+            }
+            foreach (String s in leftListBox.SelectedItems)
+            {
+                indexes.Add(leftListBox.Items.IndexOf(s));
+            }
+            for (int i = indexes.Count -1; i >= 0; i--)
+            {
+                leftListBox.Items.RemoveAt(indexes[i]);
+            }
+        }
+
+        private void rightToLeftArrow_Click(object sender, RoutedEventArgs e)
+        {
+            List<int> indexes = new List<int>();
+            foreach (String s in rightListBox.SelectedItems)
+            {
+                leftListBox.Items.Add(s);
+            }
+            foreach (String s in rightListBox.SelectedItems)
+            {
+                indexes.Add(rightListBox.Items.IndexOf(s));
+            }
+            for (int i = indexes.Count -1; i >= 0; i--)
+            {
+                rightListBox.Items.RemoveAt(indexes[i]);
+            }
+
+        }
+
+        private void leftClear_Click(object sender, RoutedEventArgs e)
+        {
+            leftListBox.Items.Clear();
+        }
+
+        private void rightClear_Click(object sender, RoutedEventArgs e)
+        {
+            rightListBox.Items.Clear();
+        }
+
+
+    }
+}
+    
+
---
+++ Opgave6_8/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Opgave6_8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void north_Checked(object sender, RoutedEventArgs e)
+        {
+            rdbResult.Content = "North Checked";
+        }
+
+        private void east_Checked(object sender, RoutedEventArgs e)
+        {
+            rdbResult.Content = "East Checked";
+        }
+
+        private void south_Checked(object sender, RoutedEventArgs e)
+        {
+            rdbResult.Content = "South Checked";
+        }
+
+        private void west_Checked(object sender, RoutedEventArgs e)
+        {
+            rdbResult.Content = "West Checked";
+        }
+
+
+        //Checked
+        private void lemon_Checked(object sender, RoutedEventArgs e)
+        {
+            updateCheckBoxResult();
+        }
+
+        private void orange_Checked(object sender, RoutedEventArgs e)
+        {
+            updateCheckBoxResult();
+        }
+
+        private void banana_Checked(object sender, RoutedEventArgs e)
+        {
+            updateCheckBoxResult();
+        }
+
+        //Unchecked
+        private void lemon_Unchecked(object sender, RoutedEventArgs e)
+        {
+            updateCheckBoxResult();
+        }
+
+        private void orange_Unchecked(object sender, RoutedEventArgs e)
+        {
+            updateCheckBoxResult();
+        }
+
+        private void banana_Unchecked(object sender, RoutedEventArgs e)
+        {
+            updateCheckBoxResult();
+        }
+
+
+
+
+
+        //Update
+        private void updateCheckBoxResult()
+        {
+            List<String> concatString = new List<string>();
+            String status = "";
+            if (lemon.IsChecked == true)
+            {
+                concatString.Add("Lemon,");
+            } else 
+            { 
+                concatString.Remove("Lemon");
+            }
+            if (orange.IsChecked == true)
+            {
+                concatString.Add(" Orange,");
+            } else
+            {
+                concatString.Remove(" Orange,");
+            }
+            if (banana.IsChecked == true)
+            {
+                concatString.Add(" Banana,");
+            } else
+            {
+                concatString.Remove(" Banana,");
+            }
+            for (int i = 0; i < concatString.Count; i++)
+            {
+                status += concatString[i];
+            }
+            if (status.Contains(","))
+                status = status.Remove(status.Length - 1);
+            cBoxResult.Content = status;
+        }
+
+
+    }
+}
