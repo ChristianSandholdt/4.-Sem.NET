6a0642c956c9b980ff9eaa35ba53deda1ea2dab8
ChristianSandholdt <cks2407@gmail.com>
2023-09-22 10:55:50 +0200

    Lektion9

---
+++ Opgave9_1/CalculatorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Opgave9_1
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("TimeCalculator");
+        }
+        [HttpPost]
+        public IActionResult Index(IFormCollection formCollection)
+        {
+            int hours = Int32.Parse(formCollection["Hours"]);
+            int minutes = Int32.Parse(formCollection["Minutes"]);
+            int seconds = Int32.Parse(formCollection["Seconds"]);
+            ViewBag.Hours = hours;
+            ViewBag.Minutes = minutes;
+            ViewBag.Seconds = seconds;
+
+
+            TimeSpan ts = new TimeSpan(0,hours, minutes, seconds);
+            double total = ts.TotalSeconds;
+
+            ViewBag.Total = total;
+            return View("TimeCalculatorResult");
+        }
+    }
+}
---
+++ Opgave9_1/SimpleCalculatorController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace Opgave9_1
+{
+    public class SimpleCalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("SimpleCalculator");
+        }
+        [HttpPost]
+        public IActionResult Index(IFormCollection formcollection)
+        {
+            int number1 = Int32.Parse(formcollection["number1"]);
+            int number2 = Int32.Parse(formcollection["number2"]);
+            double result = 0;
+            string operatorSymbol = formcollection["operator"];
+            if (number1 != null && number2 != null)
+            {
+                if (operatorSymbol == "/")
+                {
+                    if (number1 == 0 || number2 == 0)
+                    {
+                        ViewBag.result = "Division by zero";
+                        return View("SimpleCalculator");
+                    }
+                    else
+                    {
+                        result = number1 / number2;
+                    }
+                } else if (operatorSymbol == "*") {
+                    result = number1 * number2;
+                } else if (operatorSymbol == "-")
+                {
+                    result = number1 - number2;
+                } else
+                {
+                    result = number1 + number2; 
+                }
+            }
+            ViewBag.result = result.ToString();
+            return View("SimpleCalculator");
+        }
+    }
+}
