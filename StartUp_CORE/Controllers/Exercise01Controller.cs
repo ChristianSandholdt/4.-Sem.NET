90118f519435286589d21e5b6c20bfebb0ee6d2d
ChristianSandholdt <100411100+ChristianSandholdt@users.noreply.github.com>
2023-09-25 23:03:15 +0200

    fil til lektion 10

---
+++ StartUp_CORE/Controllers/Exercise01Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson02_Startup.Controllers
+{
+    public class Exercise01Controller : Controller
+    {
+        
+
+        public ActionResult Index()
+        {
+            // create a new product object with instance name glass
+            Product glass = new Product("Wine glass", 160.50);
+            glass.ImageUrl = "grandcru.jpg";
+            ViewBag.Glass = glass;
+			
+            return View();
+        }
+
+    }
+}
